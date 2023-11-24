4af5f18fa5d145bcf6967b09a505d7b1566d00dc
Pigge <pigge3067@gmail.com>
2023-11-24 12:39:43 +0100

    Update

--- forvirradforelasare/forvirradforelasare.py
+++ forvirradforelasare/forvirradforelasare.py
@@ -6,8 +6,8 @@ skip = 0
 empty = 0
 
 for i in range(1, N):
-    cur = int(lek[i])
-    pre = int(lek[i-1])
+    cur = int(les[i])
+    pre = int(les[i-1])
     if cur > pre:
         skip += abs(pre - cur)
     elif cur < pre:
